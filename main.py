f29fd26aa21bb54146538325db7ba267a7145464
Katrin <bla@blabla.is>
2021-04-25 00:58:52 -0700

    SubFolder line edit Button Created, doesn't create sub foldcers yet

--- main.py
+++ main.py
@@ -13,6 +13,7 @@ class MainWindow(QtWidgets.QMainWindow):
         self.setWindowTitle("Katrins Amazing Folder Generator")
         self.generalLayout = QtWidgets.QVBoxLayout()
         self.path_layout = QtWidgets.QFormLayout()
+        self.folder_layout = QtWidgets.QGridLayout()
 
         self.centralWidget = QtWidgets.QWidget()
         self.setCentralWidget(self.centralWidget)
@@ -21,28 +22,34 @@ class MainWindow(QtWidgets.QMainWindow):
         # It is best to always name variables with a descriptive name, this helps down the line when you have very large
         # modules to both keep the code clean and referencing when you need to call different
         self.root_path_le = QtWidgets.QLineEdit()
+        self.root_sub_folder_le = QtWidgets.QLineEdit()
 
         # We need to create a list to store the line edits so each one remains its own object
-        self.sub_path_line_edits = list()
+        self.path_line_edits = list()
+        self.folder_line_edits = list()
 
-        self.file_path()
+        self.folder_path()
 
-        self.button1 = QtWidgets.QPushButton("New Folder")
-        self.generalLayout.addWidget(self.button1)
-        self.button1.pressed.connect(self.new_folder)
+        self.newLeButton = QtWidgets.QPushButton("New Folder")
+        self.generalLayout.addWidget(self.newLeButton)
+        self.newLeButton.pressed.connect(self.new_folder)
 
-        self.button2 = QtWidgets.QPushButton("Create Folders")
-        self.generalLayout.addWidget(self.button2)
-        self.button2.pressed.connect(self.create_files)
+        self.subLeButton = QtWidgets.QPushButton("New Sub Folder")
+        self.generalLayout.addWidget(self.subLeButton)
+        self.subLeButton.pressed.connect(self.new_sub_folder)
+
+
+        self.createButton = QtWidgets.QPushButton("Create Folders")
+        self.generalLayout.addWidget(self.createButton)
+        self.createButton.pressed.connect(self.create_files)
+
 
         self.generalLayout.addLayout(self.path_layout)
 
         self.new_folder()
-        self.new_folder()
-        self.new_folder()
-        self.test()
 
-    def file_path(self):
+
+    def folder_path(self):
         self.root_path_le = QtWidgets.QLineEdit()
         self.root_path_le.setFixedHeight(20)
         self.root_path_le.setAlignment(QtCore.Qt.AlignRight)
@@ -53,12 +60,30 @@ class MainWindow(QtWidgets.QMainWindow):
         # the previously defined item.  In addition any variable/attribute that is getting defined as self.*** should
         # be done in the init method.  So once we have defined the line we need to add it to the list we created in the
         # init so it isn't destroyed once this method is complete
+        path_le = QtWidgets.QLineEdit()
+        path_le.setFixedHeight(20)
+        path_le.setAlignment(QtCore.Qt.AlignRight)
+        foldername = "  Folder: "
+
+
+        self.path_layout.addRow(foldername, path_le)
+        self.path_line_edits.append(path_le)
+
+
+    def new_sub_folder(self):
         sub_path_le = QtWidgets.QLineEdit()
         sub_path_le.setFixedHeight(20)
         sub_path_le.setAlignment(QtCore.Qt.AlignRight)
-        filename = "File: "
-        self.path_layout.addRow(filename, sub_path_le)
-        self.sub_path_line_edits.append(sub_path_le)
+        subfoldername = "       SubFolder: "
+
+        self.path_layout.addRow(subfoldername, sub_path_le)
+        self.path_line_edits.append(sub_path_le)
+
+
+    def refreshAll(self):
+        #refreshing window. Removes all existing folder lineEdits and starts from scratch.
+        pass
+
 
     def create_files(self):
         # Generally I convert anything from a line into str just to be safe as QT will sometimes return a QString which
@@ -66,8 +91,7 @@ class MainWindow(QtWidgets.QMainWindow):
         root_path = str(self.root_path_le.text())
         # Here we can now iterate over the list of widgets we created as previously it was only referencing a single
         # line edit
-        for sub_path_le in self.sub_path_line_edits:
-            filename = sub_path_le.text()
+        for sub_path_le in self.path_line_edits:
             # I changed this to the os.path.join since it is IMO the best way to consolidate items into a path
             new_path = os.path.join(root_path, str(sub_path_le.text())).replace('\\', '/')
 
@@ -77,17 +101,25 @@ class MainWindow(QtWidgets.QMainWindow):
                     os.makedirs(new_path)
                 except (IOError, PermissionError):
                     print('Attempt to create directory failed:', new_path)
+                    traceback.print_exc()
                     # The below is helpful to see exactly what the error encountered was
                     # traceback.print_exc()
+
+
+
+
             else:
                 # For debug prints like this it is good to add the data regarding what it tried so that in the event
                 # this is not expected behaviour you will know exactly what it was referring to
                 print("folder already exists:", new_path)
 
-    def test(self):
-        for i in range(self.generalLayout.count()):
-            item = self.generalLayout.itemAt(i)
-            print(item)
+
+
+
+
+
+
+
 
 
 def main():
