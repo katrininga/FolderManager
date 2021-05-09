acaf06fa79d404f69fdef66e11c78eb71cf263b0
Katrin <bla@blabla.is>
2021-05-09 00:10:44 -0700

    Testing

--- Test.py
+++ Test.py
@@ -1,19 +1,14 @@
 import yaml
 
-folder_dict = dict()
+folder_dict = {1 : "einn", 2 : "Tveir", 3 : "thrir", "nester" : {1 : "one", 2 : "two"}}
 
-folder_dict["blah"] = 1
-folder_dict["blu"] = 2
+parent_folder = list(folder_dict)[-1]
 
-folder_dict["slab"] = ""
-folder_dict["flab"] = ""
-folder_dict["ham"] = ""
-last = list(folder_dict)[-1]
+subfolder1 = "I'm a sub"
+subfolder2 = "I'm also a sub"
 
-folder_dict[last] = 5
+print(folder_dict["nester"][1])
 
-with open("Test.yaml", "w") as yaml_file:
-    yaml.dump(folder_dict,yaml_file, default_flow_style=False)
+if parent_folder == "nester":
 
-
-print(folder_dict)
+print(parent_folder)
